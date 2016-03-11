f3e6515c2eca29b9366158f56256eb380c2a64a3
fahrizal95 <fahrizal7343@gmail.com>
2016-03-11 22:41:56 +0700

    Tugas07

---
+++ Tugas07-PilihGambar/app/src/main/java/id/ac/unsyiah/elektro/mobile/tugas07_pilihgambar/FragKiri.java
@@ -0,0 +1,22 @@
+package id.ac.unsyiah.elektro.mobile.tugas07_pilihgambar;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FragKiri extends Fragment {
+
+    public FragKiri() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_kiri, container, false);
+    }
+}
---
+++ Tugas07-PilihGambar/app/src/main/java/id/ac/unsyiah/elektro/mobile/tugas07_pilihgambar/MainActivity.java
@@ -0,0 +1,93 @@
+package id.ac.unsyiah.elektro.mobile.tugas07_pilihgambar;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ImageView gambar = (ImageView) findViewById(R.id.imageView);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                switch (position) {
+                    case 0:
+                        gambar.setImageResource(R.drawable.mobil);
+                        break;
+                    case 1:
+                        gambar.setImageResource(R.drawable.sepedamotor);
+                        break;
+                    case 2:
+                        gambar.setImageResource(R.drawable.becakmotor);
+                        break;
+                    case 3:
+                        gambar.setImageResource(R.drawable.becakdayung);
+                        break;
+                    case 4:
+                        gambar.setImageResource(R.drawable.sepeda);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+                // sometimes you need nothing here
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
