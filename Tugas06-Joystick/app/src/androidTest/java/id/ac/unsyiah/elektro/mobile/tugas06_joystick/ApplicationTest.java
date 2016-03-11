43cbdd3935ca85a7aa1ff377d44e37c45de9de90
fahrizal95 <fahrizal7343@gmail.com>
2016-03-11 20:52:08 +0700

    Tugas06

---
+++ Tugas06-Joystick/app/src/androidTest/java/id/ac/unsyiah/elektro/mobile/tugas06_joystick/ApplicationTest.java
@@ -0,0 +1,13 @@
+package id.ac.unsyiah.elektro.mobile.tugas06_joystick;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
