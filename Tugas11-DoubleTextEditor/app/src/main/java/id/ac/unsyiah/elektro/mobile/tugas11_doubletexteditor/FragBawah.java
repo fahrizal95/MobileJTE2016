e5a6d2cb6797292deaf3836cf80ed5660cbeb0f5
fahrizal95 <fahrizal7343@gmail.com>
2016-03-25 22:01:00 +0700

    Tugas 25-03-2016

---
+++ Tugas11-DoubleTextEditor/app/src/main/java/id/ac/unsyiah/elektro/mobile/tugas11_doubletexteditor/FragBawah.java
@@ -0,0 +1,22 @@
+package id.ac.unsyiah.elektro.mobile.tugas11_doubletexteditor;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FragBawah extends Fragment {
+
+    public FragBawah() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag_bawah, container, false);
+    }
+}
